faa6d57b7e8a10292bf4d2d72c16cd49c4e01df1
Jitendra <jitendra.khare123@gmail.com>
2022-09-03 15:23:37 +0530

    Changes responce

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 app.get("/getUser", (req, res)=>{
   console.log("This is test for edit");
-  res.send("This is  Jitendra khare");
+  res.send("Hi Jitendra khare");
 });
 app.listen(4000);
