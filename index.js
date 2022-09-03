789f49881a011d1326eb903913b38cdfa9ddb626
Jitendra <jitendra.khare123@gmail.com>
2022-09-03 14:44:24 +0530

    Change text in responce

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 app.get("/getUser", (req, res)=>{
   console.log("This is test for edit");
-  res.send("Hello Jitendra khare");
+  res.send("This is  Jitendra khare");
 });
 app.listen(4000);
